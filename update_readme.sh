bbbf6cefc5b7a19cb4fe4cbed448bc783f2e8f68
Kim Thoenen <kim@smuzey.ch>
2013-10-03 17:58:07 +0200

    Update

--- update_readme.sh
+++ update_readme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git checkout gh-pages
 
 for branch in start step-1 step-2 step-3 step-4 step-5 step-6
 do
@@ -6,9 +7,12 @@ do
 	text="$text\n\n$(cat README.md)"
 done
 
+echo -e "$text" > publish_me.md
 echo -e "$text" | pandoc -f markdown -t html -o publish_me.html
 
 #git checkout gh-pages
 #git add index.html
 #git commit -m "updated README"
 #git push
+
+git checkout gh-pages
