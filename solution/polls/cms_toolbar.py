0f15e20430f7ffb11432a1d4c28edd272659a497
Kim Thoenen <kim@smuzey.ch>
2013-10-03 15:51:31 +0200

    Added solution for step 4

---
+++ solution/polls/cms_toolbar.py
@@ -0,0 +1,17 @@
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+from cms.toolbar_pool import toolbar_pool
+from cms.toolbar.items import Break
+from cms.cms_toolbar import ADMIN_MENU_IDENTIFIER, ADMINISTRATION_BREAK
+from cms.toolbar_base import CMSToolbar
+
+@toolbar_pool.register
+class PollToolbar(CMSToolbar):
+
+    def populate(self):
+        admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER, _('Site'))
+        position = admin_menu.find_first(Break, identifier=ADMINISTRATION_BREAK)
+        menu = admin_menu.get_or_create_menu('poll-menu', _('Polls'), position=position)
+        url = reverse('admin:polls_poll_changelist')
+        menu.add_sideframe_item(_('Poll overview'), url=url)
+        admin_menu.add_break('poll-break', position=menu)
