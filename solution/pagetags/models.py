3bcea1689b81fde796c23882ad84291dcab6c30a
Kim Thoenen <kim@smuzey.ch>
2013-10-11 12:10:46 +0200

    Removing solution because new one will be added

--- solution/pagetags/models.py
+++
@@ -1,8 +0,0 @@
-from cms.extensions import PageExtension, extension_pool
-from taggit.managers import TaggableManager
-
-
-class PageTag(PageExtension):
-    tags = TaggableManager()
-
-extension_pool.register(PageTag)
